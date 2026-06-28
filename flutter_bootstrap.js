437c87e6b16465c0bc6d084fd598a29166172b68
theahmedhany <a7medhanyshokry@gmail.com>
2026-06-29 00:59:19 +0300

    Portfolio Update.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2526190946" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1163657646" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
